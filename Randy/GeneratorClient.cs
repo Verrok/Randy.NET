647f3310a07ae98d01ef50ca9845ec3623b35cbd
Verrochek <moranmr8@gmail.com>
2019-12-26 13:45:25 +0300

    * documented methods in IGeneratorClient * mocked methods in IGeneratorClient

--- Randy/GeneratorClient.cs
+++ Randy/GeneratorClient.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading;
@@ -65,7 +66,7 @@ namespace Randy
             throw new NotImplementedException();
         }
 
-        public GetIntegerResponse GetIntegersAsync(int count, int min, int max, bool replacement = true, int @base = 10,
+        public Task<GetIntegerResponse> GetIntegersAsync(int count, int min, int max, bool replacement = true, int @base = 10,
             CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
@@ -76,32 +77,41 @@ namespace Randy
             throw new NotImplementedException();
         }
 
-        public GetIntegerSequencesRequest GetIntegerSequencesAsync()
+        public Task<GetIntegerSequencesRequest> GetIntegerSequencesAsync(int count, IEnumerable<int> length, IEnumerable<int> min, IEnumerable<int> max, IEnumerable<bool> replacement,
+            IEnumerable<int> @base, CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
 
-        public GetIntegerSequencesRequest GetIntegerSequences()
+        public Task<GetIntegerSequencesRequest> GetIntegerSequencesAsync(int count, int length, int min, int max, bool replacement = true, int @base = 10,
+            CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
 
-        public GetDecimalFractionsResponse GetDecimalFractionsAsync()
+        public GetIntegerSequencesRequest GetIntegerSequences(int count, IEnumerable<int> length, IEnumerable<int> min, IEnumerable<int> max,
+            IEnumerable<bool> replacement, IEnumerable<int> @base)
         {
             throw new NotImplementedException();
         }
 
-        public GetDecimalFractionsResponse GetDecimalFractions()
+        public Task<GetDecimalFractionsResponse> GetDecimalFractionsAsync(int count, int decimalPlaces, bool replacement = true,
+            CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
 
-        public GetGaussiansResponse GetGaussiansAsync()
+        public GetDecimalFractionsResponse GetDecimalFractions(int count, int decimalPlaces, bool replacement = true)
         {
             throw new NotImplementedException();
         }
 
-        public GetGaussiansResponse GetGaussians()
+        public Task<GetGaussiansResponse> GetGaussiansAsync(int count, int mean, int deviation, int digits, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public GetGaussiansResponse GetGaussians(int count, int mean, int deviation, int digits)
         {
             throw new NotImplementedException();
         }
