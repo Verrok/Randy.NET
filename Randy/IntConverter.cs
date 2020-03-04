fc794952589b5350bce871b103391f96f976581a
Verrochek <moranmr8@gmail.com>
2020-03-04 08:05:14 +0300

    added: int converter for different base

---
+++ Randy/IntConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Randy
+{
+    public class IntConverter : JsonConverter<int>
+    {
+
+        private readonly int _base;
+
+
+        public IntConverter(int @base)
+        {
+            _base = @base;
+        }
+        
+        
+        public override int Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return Convert.ToInt32(reader.GetString(), _base);
+        }
+
+        public override void Write(Utf8JsonWriter writer, int value, JsonSerializerOptions options)
+        {
+            writer.WriteNumberValue(value);
+        }
+    }
+}
