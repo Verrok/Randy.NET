43934c26c2178df3c8333bbd45fa393e7bd60181
Verrochek <moranmr8@gmail.com>
2020-06-09 21:21:58 +0300

    added: NUKE build system

---
+++ build/Build.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.Execution;
+using Nuke.Common.Git;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+[CheckBuildProjectConfigurations]
+[UnsetVisualStudioEnvironmentVariables]
+class Build : NukeBuild
+{
+
+    public static int Main () => Execute<Build>(x => x.Pack);
+
+
+    [Solution] readonly Solution Solution;
+
+    AbsolutePath OutputDirectory => RootDirectory / "output";
+    
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            EnsureCleanDirectory(OutputDirectory);
+        });
+
+    Target Restore => _ => _
+        .DependsOn(Clean)
+        .Executes(() =>
+        {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
+        });
+
+    Target Pack => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            DotNetPack(s => s
+                .SetProject(Solution.GetProject("Randy"))
+                .SetOutputDirectory(OutputDirectory));
+        });
+
+}
